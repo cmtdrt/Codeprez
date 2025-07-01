2d1eb2ab968a8f8e2ae00dfff9ff9a870935fdef
simon <simon.louis638@gmail.com>
2025-07-01 13:55:43 +0200

    height + width for slides

--- frontend/src/components/Slides.vue
+++ frontend/src/components/Slides.vue
@@ -98,12 +98,14 @@ function loadCustomCSS(cssContent) {
 
 .slide {
   margin: 0;
-  padding: 0;
-  width: 100%;
-  height: 80vh;
+  padding: 2rem;
+  width: 100vw;
+  height: 90vh;
+  box-sizing: border-box;
   display: flex;
   align-items: center;
   justify-content: center;
+  overflow: auto;
 }
 
 .nav {
